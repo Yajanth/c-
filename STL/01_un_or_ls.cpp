fce35578a55e2d19c0d105c9b56231d92e5c76bb
Yajanth <yajanthrr@gmail.com>
2024-05-03 00:47:21 +0530

    vectors

--- STL/01_un_or_ls.cpp
+++ STL/01_un_or_ls.cpp
@@ -1,9 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-  pair<int,int> p={1,10};
-  cout << p.first << "," << p.second << endl;
-  pair<int,pair<int,int>> q={1,{20,50}};
-  cout << q.second.second << endl;
+ pair<int,float> p={10,5};
+ cout << p.first;
+// Pair inside pair
+ pair<int, pair<int,pair<int,int>>> q={10,{10,{5,6}}};
+ cout << q.second.first << endl;
+
   return 0;
 }
--- STL/02_vector.cpp
+++ STL/02_vector.cpp
@@ -1,12 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main (){
+  
   vector<int> v(5,10);
   cout<< v[4] << endl;
+  v.push_back(9);
+
   for (int i = 0; i < v.size(); i++) {
-   cout << v[i] << ","; 
- 
-  }
-                     
+   cout << v[i] << ",";  
+  }cout << endl;
   return 0;
+
 }
