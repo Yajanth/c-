5e5209dacf496aedd16b07d1a671eb9e432ea9aa
Yajanth <yajanthrr@gmail.com>
2024-05-04 23:28:41 +0530

    update

--- STL/02_vector.cpp
+++ STL/02_vector.cpp
@@ -3,11 +3,12 @@ using namespace std;
 int main (){
   
   vector<int> v(5,10);
+  vector<int> v1(5); // creates a vector of 5 zeros
   cout<< v[4] << endl;
   v.push_back(9);
 
-  for (int i = 0; i < v.size(); i++) {
-   cout << v[i] << ",";  
+  for (int i = 0; i < v1.size(); i++) {
+   cout << v1[i] << ",";  
   }cout << endl;
   return 0;
 
