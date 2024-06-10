05e67b257336d81e7bdd867776e4434f127e0203
Yajanth <yajanthrr@gmail.com>
2024-06-10 23:38:21 +0530

    rev_arr

---
+++ recurssion/07_reverse_arr.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void revarr(int arr[],int n){
+
+  int rev_arr[n];
+  for (int i = 0; i < n; i++) {
+    rev_arr[i]=arr[n-i-1];
+    cout << rev_arr[i] << endl;
+  }
+}
+int main(){
+
+  int arr[5]={1,2,3,4,5};
+  revarr(arr,5);
+  return 0;
+}
